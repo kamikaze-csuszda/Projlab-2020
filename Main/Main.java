1f176d1a7c290633732a55871dc608512aaf1e57
vivivaradi <varadivivien98@gmail.com>
2020-03-29 11:40:50 +0200

    Első kiírás kész, felhasználó válasza eltárolva. Szerintem kellene egy skeleton.java is, ami külön kezeli majd a teszteseteket.

---
+++ Main/Main.java
@@ -0,0 +1,35 @@
+package Main;
+
+
+import java.util.Scanner;
+
+public class Main{
+
+    public static void main(String[] args)
+    {
+        System.out.println("Válassz egy tesztesetet! Gépeld be a futtatni kívánt eset számát, és egy entert!\n" +
+                "1) Eszkimó egy jégtábláról stabil jégtáblára lép\n" +
+                "2) Eszkimó egy jégtábláról instabil jégtáblára lép\n" +
+                "3) Eszkimó lukas jégtáblára lép, vízbe esik (túléli)\n" +
+                "4) Eszkimó lukas jégtáblára lép, vízbe esik (meghal, játék vége)\n" +
+                "5) Karakter hóviharba kerül\n" +
+                "6) Karakter igluban vészeli át a hóvihart\n" +
+                "7) Sarkkutató megnézi a szomszédos mező teherbírását\n" +
+                "8) Eszkimó iglut épít\n" +
+                "9) Eszkimó havat takarít\n" +
+                "10) Eszkimó havat lapátol\n" +
+                "11) Eszkimó feltöri a jeget\n" +
+                "12) Eszkimó felvesz egy tárgyat\n" +
+                "13) Eszkimó elfogyaszt egy élelmet\n" +
+                "14) Eszkimó összerakja a pisztolyt\n" +
+                "15) Eszkimó összerakja a pisztolyt\n" +
+                "16) Eszkimó átad eszközt a sarkkutatónak\n");
+        Scanner in = new Scanner(System.in);
+        int ans = in.nextInt();
+        switch (ans)
+        {
+            case 1:
+
+        }
+    }
+}
