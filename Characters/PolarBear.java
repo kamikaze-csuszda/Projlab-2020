73154b6e97a72815a0619d4136109d01121eabba
vivivaradi <varadivivien98@gmail.com>
2020-04-25 16:44:56 +0200

    PolarBear elvileg kész

--- Characters/PolarBear.java
+++ Characters/PolarBear.java
@@ -1,11 +1,27 @@
 package Characters;
 
+import Ice.Ice;
+import Startegy.NoBear;
+import Strategy.Bear;
+
 public class PolarBear implements Movable {
 
+	private Ice ice;
+
+	/**
+	 * A medve atmozog a megadott szomszedos mezore, es a strategiakat is ennek megfeleloen allitja.
+	 * A vegen pedig jelzi, hogy lepett, azaz ha van vedtelen karakter a mezon, akkor vege a jateknak.
+	 * @param d
+	 */
 	@Override
 	public void move(int d) {
-		// TODO Auto-generated method stub
-		
+		NoBear nb = new NoBear();
+		Ice neighbour = ice.getNeighbour(d);
+		ice.setBearStrategy(nb);
+		setIce(neighbour);
+		Bear b = new Bear();
+		neighbour.setBearStrategy(b);
+		neighbour.bearHere();
 	}
 	
 	
