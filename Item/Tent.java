77f7a0c0ff5ad92353080c9cdbfd346f7880e9d8
vivivaradi <varadivivien98@gmail.com>
2020-04-25 15:28:22 +0200

    Itemek javítva, Tent megírva, Itemnek alapértelmezett konstruktor írva

--- Item/Tent.java
+++ Item/Tent.java
@@ -1,11 +1,37 @@
+package Item;
+
+import Ice.*;
+import Characters.Character;
+import Strategy.TentStrategy;
+import Strategy.NoIglooStrategy;
+
 
 public class Tent extends Item {
 
+    /**
+     * Alapertelmezett konstruktor.
+     */
     public Tent(){
-        super(-1);
+        super();
     }
 
+    /**
+     * Beallitja a mezo strategiajat arra, hogy sator talalhato rajta.
+     */
     public void use(){
+        TentStrategy ts = new TentStrategy();
+        Character c = this.getCharacter();
+        Ice i = c.getIce();
+        i.setIglooStrategy(ts);
+    }
 
+    /**
+     * Beallitja a mezo strategiajat arra, hogy nincs rajta sator.
+     */
+    public void discard(){
+        NoIglooStrategy nig = new NoIglooStrategy();
+        Character c = this.getCharacter();
+        Ice i = c.getIce();
+        i.setIglooStrategy(nig);
     }
 }
