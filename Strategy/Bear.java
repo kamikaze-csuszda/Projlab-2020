4c06fb6a17c934b7b45bcdcb26ce0f11793f6464
vivivaradi <varadivivien98@gmail.com>
2020-04-25 16:23:01 +0200

    NoBear szekvenciának megfelelő, Beart ki kell találni mert a szekvencia nem jó

--- Strategy/Bear.java
+++ Strategy/Bear.java
@@ -1,5 +1,7 @@
 package Strategy;
 
 public class Bear {
-
+    public void bearHere(Ice i){
+        //TODO: szekvencia nem lesz jo
+    }
 }
--- Strategy/NoBear.java
+++ Strategy/NoBear.java
@@ -1,5 +1,11 @@
 package Strategy;
 
 public class NoBear {
+    /**
+     * Mivel nincs a mezon medve, igy nem csinal semmit.
+     * @param i a jegmezo amit nezunk
+     */
+    public void bearHere(Ice i){
 
+    }
 }
