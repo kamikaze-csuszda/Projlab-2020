fb1598af81607eabe1d5eb5692ed3da99af0a8da
vivivaradi <varadivivien98@gmail.com>
2020-05-17 19:25:39 +0200

    elvileg felső menubar kész, hiányozhatnak belőle akciók amik nem jutottak eszembe

--- Graphics/GameFrame.java
+++ Graphics/GameFrame.java
@@ -4,11 +4,14 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 
-import javax.swing.JPanel;
+import javax.swing.*;
 
 public class GameFrame extends MyFrame implements UpdateInterface
 {
-
+	private JMenuBar menuBar;
+	private JMenu characterMenu, actionMenu;
+	private ArrayList<JMenuItem> characters;
+	private JMenuItem move, warmup, itemGive, itemPickup, itemDrop, shovel;
 	public GameFrame()
 	{
 		super("Jegmezo - Jatek");
@@ -23,6 +26,22 @@ public class GameFrame extends MyFrame implements UpdateInterface
 		jp2.setBackground(Color.cyan);
 		add(jp1, BorderLayout.EAST);
 		add(jp2, BorderLayout.WEST);
+
+		menuBar = new JMenuBar();
+		characterMenu = new JMenu("Karakterek");
+		actionMenu = new JMenu("Akciok");
+
+		for(String key : Game.getInstance().getObjects().keySet()){
+			if (Game.getInstance().getObjects().get(key) instanceof Character)
+				characters.add(new JMenuItem(key));
+		}
+		move = new JMenuItem("Mozgas");
+		warmup = new JMenuItem("Melegedes");
+		itemGive = new JMenuItem("Eszkoz atadas");
+		itemPickup = new JMenuItem("Eszkoz felvetel");
+		itemDrop = new JMenuItem("Eszkoz eldobasa");
+		shovel = new JMenuItem("Ho eltakaritas");
+
 		setBackground(Color.cyan);
 		setDefaultCloseOperation(this.EXIT_ON_CLOSE);
 		
